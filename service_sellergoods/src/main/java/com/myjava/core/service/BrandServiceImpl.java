bd830e3185531b75b810a1c67ef935a6a06fa7b7
guochun <1076294651@qq.com>
2022-06-10 15:12:13 +0800

    6. service端添加service注解使方法可以被识别到

--- service_sellergoods/src/main/java/com/myjava/core/service/BrandServiceImpl.java
+++ service_sellergoods/src/main/java/com/myjava/core/service/BrandServiceImpl.java
@@ -1,9 +1,11 @@
 package com.myjava.core.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
+
+@Service
 public class BrandServiceImpl implements BrandService {
     @Override
     public String findAll() {
-
         return "远程调用 : findAll方法";
     }
 }
