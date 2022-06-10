7785ed28fb568a3fd9c430b2f481a9133dee8777
guochun <1076294651@qq.com>
2022-06-10 13:32:58 +0800

    4. 创建服务端-销售商品,负责实现interface模块中的接口

---
+++ service_sellergoods/src/main/java/com/myjava/core/service/BrandServiceImpl.java
@@ -0,0 +1,9 @@
+package com.myjava.core.service;
+
+public class BrandServiceImpl implements BrandService {
+    @Override
+    public String findAll() {
+
+        return "远程调用 : findAll方法";
+    }
+}
