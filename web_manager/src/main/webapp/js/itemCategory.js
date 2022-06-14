a902ea9441024b3e1de8b8d0115ae8a8d09e34b8
guochun <1076294651@qq.com>
2022-06-14 21:21:12 +0800

    65. 实现面包屑导航(未解决对象数组删除问题)

--- web_manager/src/main/webapp/js/itemCategory.js
+++ web_manager/src/main/webapp/js/itemCategory.js
@@ -2,21 +2,55 @@ new Vue({
     el: "#app",
     data: {
         categoryList: [],
+        breadList: [{
+            id: 0,
+            name: "顶级分类列表"
+        }],//面包屑导航列表
     },
     methods: {
-        selectCateByParentId: function (id) {
-            _this = this;
+        selectCateByParentId: function (id, name) {
+            var _this = this;
+            if (id != 0) {
+                this.breadList.push({id, name});
+            }
             axios.get("/itemCate/findByParentId/" + id + ".do")
                 .then(function (response) {
                     //取服务端响应的结果
-                    _this.categoryList = response.data;
-                    console.log(response.data);
+                    let data = response.data;
+                    if (data === null) {
+                        alert("没有下一级了")
+                        let parentId = _this.categoryList[0].parentId;
+                        _this.selectCateByParentId(parentId)
+                    } else {
+                        _this.categoryList = data;
+                    }
                 }).catch(function (reason) {
                 console.log(reason);
             })
         },
+        breadHandler: function (id) {
+            if (id === 0) {
+                this.selectCateByParentId(0);
+                return;
+            }
+            var _this = this;
+            var flag = false;
+            this.breadList.forEach((item) => {
+                if (flag) {
+                    let index = _this.breadList.indexOf(item);
+                    _this.breadList.splice(index, 1);
+                }
+                if (item.id === id) {
+                    flag = true;
+                }
+            })
+            console.log(_this.breadList);
+            this.selectCateByParentId(id);
+        }
     },
     created: function () {
         this.selectCateByParentId(0);
     }
 });
+
+
