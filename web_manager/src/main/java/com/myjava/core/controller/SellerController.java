e2f8c33697b60561e20c78bb64cdaff6489b1627
guochun <1076294651@qq.com>
2022-06-14 09:43:46 +0800

    56. 实现待审核商家查询功能,404原因是跨域请求,解决办法是在网页所属的web_manger工程创建controller类处理前端请求

--- web_manager/src/main/java/com/myjava/core/controller/SellerController.java
+++ web_manager/src/main/java/com/myjava/core/controller/SellerController.java
@@ -1,30 +1,22 @@
-package com.myjava.controller;
-
+package com.myjava.core.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.myjava.core.pojo.request.PageRequest;
 import com.myjava.core.pojo.request.SellerQueryContent;
 import com.myjava.core.pojo.response.PageResponse;
-import com.myjava.core.pojo.response.ResultMessage;
 import com.myjava.core.pojo.seller.Seller;
 import com.myjava.core.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/seller")
 public class SellerController {
     @Reference
-    SellerService service;
-
-    @RequestMapping("/save")
-    public ResultMessage save(@RequestBody Seller enterprise) {
-        return service.save(enterprise);
-    }
+    private SellerService service;
 
     @PostMapping("/getPage")
     public PageResponse<Seller> getPage(@RequestBody PageRequest<SellerQueryContent> request) {
---
+++ web_shop/src/main/java/com/myjava/core/controller/SellerController.java
@@ -0,0 +1,29 @@
+package com.myjava.core.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.myjava.core.pojo.request.PageRequest;
+import com.myjava.core.pojo.request.SellerQueryContent;
+import com.myjava.core.pojo.response.PageResponse;
+import com.myjava.core.pojo.response.ResultMessage;
+import com.myjava.core.pojo.seller.Seller;
+import com.myjava.core.service.SellerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/seller")
+public class SellerController {
+    @Reference
+    SellerService service;
+
+    @RequestMapping("/save")
+    public ResultMessage save(@RequestBody Seller enterprise) {
+        return service.save(enterprise);
+    }
+
+}
