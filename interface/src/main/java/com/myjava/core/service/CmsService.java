5818f11ad0d899fae38306048abb4b64513493be
guochun <1076294651@qq.com>
2022-06-26 14:35:43 +0800

    145. 页面静态化-添加静态页面模板,创建实现静态化的接口和实现类

---
+++ interface/src/main/java/com/myjava/core/service/CmsService.java
@@ -0,0 +1,22 @@
+package com.myjava.core.service;
+
+import java.util.Map;
+
+public interface CmsService {
+    /**
+     * 根据商品ID和数据生成对应的静态页面
+     *
+     * @param goodsId 商品ID
+     * @param rootMap 商品数据
+     * @throws Exception
+     */
+    public void createStaticPage(Long goodsId, Map<String, Object> rootMap) throws Exception;
+
+    /**
+     * 根据商品ID获取商品数据
+     *
+     * @param goodsId
+     * @return
+     */
+    public Map<String, Object> findGoodsData(Long goodsId);
+}
---
+++ service_page/src/main/java/com/myjava/core/service/CmsServiceImpl.java
@@ -0,0 +1,82 @@
+package com.myjava.core.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.myjava.core.dao.good.GoodsDao;
+import com.myjava.core.dao.good.GoodsDescDao;
+import com.myjava.core.dao.item.ItemCatDao;
+import com.myjava.core.dao.item.ItemDao;
+import com.myjava.core.pojo.good.Goods;
+import com.myjava.core.pojo.good.GoodsDesc;
+import com.myjava.core.pojo.item.Item;
+import com.myjava.core.pojo.item.ItemCat;
+import com.myjava.core.pojo.item.ItemQuery;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+
+import javax.servlet.ServletContext;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CmsServiceImpl implements CmsService, ServletContextAware {
+    @Autowired
+    private GoodsDao goodsDao;
+    @Autowired
+    private GoodsDescDao descDao;
+    @Autowired
+    private ItemDao itemDao;
+    @Autowired
+    private ItemCatDao catDao;
+    @Autowired
+    private FreeMarkerConfig freeMarkerConfig;
+    private ServletContext servletContext;
+
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
+    }
+
+
+    @Override
+    public void createStaticPage(Long goodsId, Map<String, Object> rootMap) throws Exception {
+        Configuration configuration = freeMarkerConfig.getConfiguration();
+        Template template = configuration.getTemplate("item.ftl");
+        String path = goodsId + ".html";
+        String realPath = getRealPath(path);
+        OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(new File(realPath)), "utf-8");
+        template.process(rootMap, out);
+        out.close();
+    }
+
+    public String getRealPath(String path) {
+        return servletContext.getRealPath(path);
+    }
+
+    @Override
+    public Map<String, Object> findGoodsData(Long goodsId) {
+        HashMap<String, Object> goodsDataMap = new HashMap<>();
+        Goods goods = goodsDao.selectByPrimaryKey(goodsId);
+        GoodsDesc goodsDesc = descDao.selectByPrimaryKey(goodsId);
+        String category1 = catDao.selectByPrimaryKey(goods.getCategory1Id()).getName();
+        String category2 = catDao.selectByPrimaryKey(goods.getCategory2Id()).getName();
+        String category3 = catDao.selectByPrimaryKey(goods.getCategory3Id()).getName();
+        ItemQuery query = new ItemQuery();
+        ItemQuery.Criteria criteria = query.createCriteria();
+        criteria.andGoodsIdEqualTo(goodsId);
+        List<Item> itemList = itemDao.selectByExample(query);
+        goodsDataMap.put("itemCat1", category1);
+        goodsDataMap.put("itemCat2", category2);
+        goodsDataMap.put("itemCat3", category3);
+        goodsDataMap.put("goods", goods);
+        goodsDataMap.put("goodsDesc", goodsDesc);
+        goodsDataMap.put("itemList", itemList);
+        return goodsDataMap;
+    }
+
+}
