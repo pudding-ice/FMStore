5a8d2c4489228a137c0bb41ecf3e3d9ecc480c57
guochun <1076294651@qq.com>
2022-06-28 10:35:24 +0800

    162. 短信登录-创建短信发送service端模块,添加依赖,spring配置文件,创建短信发送工具类和监听类

---
+++ service_sms/src/main/java/com/myjava/core/listener/SMSListener.java
@@ -0,0 +1,25 @@
+package com.myjava.core.listener;
+
+import com.myjava.core.util.SmsUtil;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class SMSListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        MapMessage map = (MapMessage) message;
+        try {
+            SmsUtil.sendSms(map.getString("mobile"),
+                    map.getString("template_code"),
+                    map.getString("sign_name"),
+                    map.getString("param"));
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        System.out.println(map);
+    }
+}
+
---
+++ service_sms/src/main/java/com/myjava/core/util/SmsUtil.java
@@ -0,0 +1,56 @@
+package com.myjava.core.util;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import com.google.gson.Gson;
+
+public class SmsUtil {
+
+    /**
+     * 发送短信
+     *
+     * @param mobile        手机号
+     * @param template_code 模板号
+     * @param sign_name     签名
+     * @param param         验证码
+     */
+
+    public static void sendSms(String mobile, String template_code, String sign_name, String param) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<your-access-key-id>", "<your-access-key-secret>");
+        /** use STS Token
+         DefaultProfile profile = DefaultProfile.getProfile(
+         "<your-region-id>",           // The region ID
+         "<your-access-key-id>",       // The AccessKey ID of the RAM account
+         "<your-access-key-secret>",   // The AccessKey Secret of the RAM account
+         "<your-sts-token>");          // STS Token
+         **/
+
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        SendSmsRequest request = new SendSmsRequest();
+        request.setSignName("阿里云短信测试");
+        request.setTemplateCode("SMS_154950909");
+        request.setPhoneNumbers("17539529616");
+        request.setTemplateParam("{\"code\":\"1234\"}");
+
+        try {
+            SendSmsResponse response = client.getAcsResponse(request);
+            System.out.println(new Gson().toJson(response));
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            System.out.println("ErrCode:" + e.getErrCode());
+            System.out.println("ErrMsg:" + e.getErrMsg());
+            System.out.println("RequestId:" + e.getRequestId());
+        }
+    }
+
+}
