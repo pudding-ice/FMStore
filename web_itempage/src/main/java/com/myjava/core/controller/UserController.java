35375ceb0539711b1578207a616a607f36afb4a6
guochun <1076294651@qq.com>
2022-07-13 10:05:35 +0800

    203. 单点登录-创建商品页web模块,整合springsecurity和CAS,添加静态页面所需静态资源

---
+++ web_itempage/src/main/java/com/myjava/core/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.myjava.core.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.myjava.core.pojo.user.User;
+import com.myjava.core.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Reference
+    UserService userService;
+
+    @RequestMapping("/getUserInfo")
+    public User getUserInfo() {
+        String username = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof String) {
+            username = (String) principal;
+        } else {
+            org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) principal;
+            username = user.getUsername();
+        }
+        User myUser = userService.getUserByName(username);
+        return myUser;
+    }
+}
